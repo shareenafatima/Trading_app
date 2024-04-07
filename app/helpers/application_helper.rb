7e798caffe4449a99ac0ca2a83d2a6fa8576c2c3
shareena fatima <shareenafatima40@gmail.com>
2024-04-07 17:52:07 +0500

    add trade bundle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 200 })
+  gravatar_id = Digest::MDS::hexdigest(user.email.downcase)
+  size = options[:size]
+  gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  image_tag(gravatar_url, alt: user.name, class: "border-radius-50")
+end
 end
